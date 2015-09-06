4d7a6c0e4b353aed21150a11c3f55b32f3fc267d
Olha Vitiaz <vitiazolha@gmail.com>
2015-09-06 19:01:29 +0300

    Rewrite first home work

--- Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Hello{
-	public static void main(String[] args) {
-		System.out.print("Enter your name : ");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Hello, " + scanner.nextLine() + "!");
-	}
-}
---
+++ HomeWorks/src/FirstHomeWork.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vitiazolha on 06.09.15.
+ */
+import java.util.Scanner;
+
+public class FirstHomeWork {
+    public static void main(String[] args) {
+        System.out.print("Enter your name : ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, " + scanner.nextLine() + "!");
+    }
+}
